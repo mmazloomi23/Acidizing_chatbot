9ec32e918711f55ba9b56d6fb7d599dd3fb3eba0
mmazloumi <m.mazloumi@sutech.ac.ir>
2025-10-03 22:37:18 +0330

    new changes for deploy

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LOGO_PATH = os.path.join(BASE_DIR, "../assets/nidc.jpg")
+APP_TITLE = ":red[NIDC] :orange[AI] :blue[Assistant]"
+APP_URL = "https://www.nidc.ir/"
+
