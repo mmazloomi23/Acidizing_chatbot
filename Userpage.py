c29168c1bb0dc3dc3706a0ec75a6c569225dd5bf
mmazloumi <m.mazloumi@sutech.ac.ir>
2025-10-03 22:51:48 +0330

    FAISS for requirements added

--- Userpage.py
+++ Userpage.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import requests
+import faiss
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from openai import OpenAI
